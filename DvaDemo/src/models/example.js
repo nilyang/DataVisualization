ae0c697b7adcff51051236bb6245844fbbc2ed66
Nil Yang <Nil Yang>
2016-11-25 01:26:29 +0800

    1.add：异步行为demo

--- DvaDemo/src/models/example.js
+++ DvaDemo/src/models/example.js
@@ -1,3 +1,4 @@
+import {query} from '../services/example'
 
 export default {
 
@@ -7,16 +8,25 @@ export default {
 
   subscriptions: {
     setup({ dispatch, history }) {
+      dispatch({type:"fetchRemote", payload:{}})
     },
   },
 
   effects: {
     *fetchRemote({ payload }, { call, put }) {
+      const {data} = yield call(query)
+      console.log("\n---fetchRemote--\n",data)
+      if(data && data.success){
+        yield put({type:"fetch", payload:data})
+      }else{
+        throw Error("fail")
+      }
     },
   },
 
   reducers: {
     fetch(state, action) {
+      console.log("\n--fetch--\n", {...state, ...action.payload})
       return { ...state, ...action.payload };
     },
   },
--- DvaDemo/src/routes/HomePage.jsx
+++ DvaDemo/src/routes/HomePage.jsx
@@ -1,13 +1,26 @@
 import React , {PropTypes} from 'react'
+import {connect} from 'dva'
 
-function HomePage() {
+function HomePage(props) {
+    console.log("\n----HomePage----\n",props)
+    function getData({data}){
+        console.log("\n---getData---\n",data)
+        return data ? data.join("-") : "";
+    }
     return (
-        <div> User Route Component</div>
+        <div> Join Remote Async Data:<h2> {getData(props.example)} </h2> </div>
     )
 }
 
 HomePage.propTypes = {
-
+    // data: PropTypes.array,
+    // success: PropTypes.bool,
 };
 
-export default HomePage;
+function mapStateToProps(state, ownPros) {
+    console.log("\n----mapStateToProps----\n",{...state})
+    return {...state};
+}
+
+
+export default connect(mapStateToProps)(HomePage);
